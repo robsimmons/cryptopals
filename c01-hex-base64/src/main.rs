46e9ff3bd54c37c28221e119726c672e10a1e4de
Rob <rjsimmon@keating.local>
2023-09-24 14:54:16 -0400

    Starter project

--- c01-hex-base64/src/main.rs
+++ c01-hex-base64/src/main.rs
@@ -7,9 +7,9 @@ fn main() {
 
     let mut guess = String::new();
 
-    io::stdin()
+    let len = io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+    println!("You guessed: {guess} {len}");
 }
